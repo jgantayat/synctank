8dcb75514339d09b80118605095741c9840397a2
jgantayat <gantayatjanmajaya@gmail.com>
2026-08-07 10:53:47 +0530

    Contract Specs updated and added AWS S3 toolkit

--- contract-platform/src/main/java/com/synctank/platform/config/S3Config.java
+++ contract-platform/src/main/java/com/synctank/platform/config/S3Config.java
@@ -1,2 +1,25 @@
-package com.synctank.platform.config;public class S3Config {
+package com.synctank.platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client(S3Props props) {
+        return S3Client.builder()
+                .endpointOverride(URI.create(props.endpoint()))
+                .region(Region.US_EAST_1)                    // MinIO ignores it; SDK requires it
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(props.accessKey(), props.secretKey())))
+                .forcePathStyle(true)                        // REQUIRED for MinIO
+                .build();
+    }
 }
--- contract-platform/src/main/java/com/synctank/platform/config/S3Props.java
+++ contract-platform/src/main/java/com/synctank/platform/config/S3Props.java
@@ -1,2 +1,6 @@
-package com.synctank.platform.config;public record S3Props() {
-}
+package com.synctank.platform.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "platform.s3")
+public record S3Props(String endpoint, String bucket, String accessKey, String secretKey) {}
